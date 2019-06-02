d288ec6c7b56e61af548277b80879625bbcdc7b8
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-06-02 18:06:02 +1000

    Health run getting stuck for some reason

--- src/mycontroller/ConserveHealthStrategy.java
+++ src/mycontroller/ConserveHealthStrategy.java
@@ -10,12 +10,19 @@ import java.util.Set;
 import tiles.MapTile;
 import tiles.TrapTile;
 import utilities.Coordinate;
+import world.World;
 
 public class ConserveHealthStrategy implements IMovementStrategy {
 	
-	
-	
+	private ArrayList<Coordinate> currPath = new ArrayList<>();
+	private boolean isHeadingToFinish = false;
 	MyAutoController control;
+	
+	// Init at same position, find next place to go
+	Coordinate currPos;
+	// The end point to find a path towards
+	Coordinate currGoingTo = null;
+	String currTileToType = "road";
 
 	public ConserveHealthStrategy(MyAutoController myAutoController) {
 		this.control = myAutoController;
@@ -23,53 +30,101 @@ public class ConserveHealthStrategy implements IMovementStrategy {
 
 	@Override
 	public void move() {
-
-		// Init at same position, find next place to go
-		Coordinate currPos = new Coordinate(control.getPosition());
-		Coordinate currGoing = new Coordinate(control.getPosition());
 		
-		
-		ArrayList<Coordinate> currPath = new ArrayList<>();
-		
+		currPos = new Coordinate(control.getPosition());
 		
 		Set<Map.Entry<Coordinate, MapTile>> viewSet = control.getView().entrySet();
 		
-		// Set the next go to
-		if (currGoing.equals(currPos)) {
-			// Go through all tiles, see if there's a parcel in the current view that we can collect
+		// If these are the same, find a new path. 
+		String trapType = "";
+		System.out.println("CurrPath: " + currPath);
+		if (currPos.equals(currGoingTo) || currGoingTo == null) {
 			for (Map.Entry<Coordinate, MapTile> item : viewSet) {
-				Coordinate tileCoord = item.getKey();
-				MapTile tile = item.getValue();
-				System.out.println(tileCoord + ", " + item.getValue().getType());
-				if (tile.getType() == MapTile.Type.TRAP) {
-					TrapTile trapTile = (TrapTile) item.getValue();
-					String trapType = trapTile.getTrap();
-					if (trapType.equals("parcel")) {
-						System.out.println("Got to the trap analysis");
-						currGoing = tileCoord;
-						currPath = findPath(currPos, tileCoord);
-					} else if (trapType.equals("health")) {
-						currGoing = tileCoord;
-						currPath = findPath(currPos, tileCoord);
-						System.out.println("Health trap");
-					}
+				Coordinate coord = item.getKey();
+				MapTile mapTile = item.getValue();
+				boolean isTrap = mapTile.getType() == MapTile.Type.TRAP;
+				if (isTrap) {
+					trapType = (String) ((TrapTile) mapTile).getTrap();
+				}
+				// Go for health first
+				if (trapType.equals("water")) {
+					System.out.println("Setting path to water tile at " + coord);
+					currPath = findPath(currPos, coord);
+					currGoingTo = currPath.get(0);
+					currTileToType = "water";
+					break;
+				} else if (trapType.equals("parcel")) {
+					System.out.println("Setting path to parcel at " + coord);
+					currPath = findPath(currPos, coord);
+					currGoingTo = currPath.get(0);
+					currTileToType = "parcel";
+					break;
 				}
 			}
+			
+			if (currPath.size() == 0 || currPath == null) {
+				System.out.println("Setting new road path");
+				// if there are no priority tiles found, just find a path
+				// currently just looks at all points and finds closest, room for optimisation
+				ArrayList<Coordinate> path;
+				ArrayList<Coordinate> bestPath = null;
+				for (Coordinate coord: generateSpiral(currPos)) {
+					if (control.unseenCoords.contains(coord)) {
+						path = findPath(currPos, coord);
+						if (path.size() > 0) {
+							if (bestPath == null || path.size() < bestPath.size()) {
+								bestPath = path;
+							}
+						}
+					}
+				}
+				currPath = bestPath;
+				currGoingTo = currPath.get(0);
+				currTileToType = "road";
+			}
+
 		}
 		
-		for (Coordinate nextCoord : currPath) {
-			System.out.println("Hello");
-			control.moveTowards(nextCoord);
-		}
 		
-		
-		
-		System.out.println(control.getView());
-//		control.applyForwardAcceleration();
-		
+		// TODO can be optimised
 		if (control.numParcelsFound() >= control.numParcels()) {
 			System.out.println("Heading to exit, have enough parcels!");
+			isHeadingToFinish = true;
+			currPath = findPath(currPos, control.finish.get(0));
 		}
+		
+		// Take next step on the path that's been set
+		control.moveTowards(currPath.get(currPath.size() - 1));
+		currPath.remove(currPath.size() - 1);
+	}
+	
+	// generates a spiral of Coordinates around a specified start in the anticlockwise direction
+	// NOTE: many points in the output array will not be valid Coordinates in the map
+	private ArrayList<Coordinate> generateSpiral(Coordinate start){
+		ArrayList<Coordinate> spiral = new ArrayList<>();
+		int dx = 1;
+		int signX = 1;
+		int dy = 1;
+		int signY = 1;
+		Coordinate temp = new Coordinate(start.toString());
+		
+		while(start.x + dx <= World.MAP_WIDTH || start.x - dx >= 0 ||
+				start.y + dy <= World.MAP_HEIGHT || start.y - dy >= 0) {
+			for (int i = 0; i < dx; i++) {
+				temp.x += signX;
+				spiral.add(new Coordinate(temp.toString()));
+			}
+			for (int i = 0; i < dy; i++) {
+				temp.y += signY;
+				spiral.add(new Coordinate(temp.toString()));
+			}
+			dx++;
+			signX *= -1;
+			dy++;
+			signY *= -1;
+		}
+		
+		return spiral;
 	}
 
 	
@@ -92,7 +147,7 @@ public class ConserveHealthStrategy implements IMovementStrategy {
 			if (curr.getCoord().equals(dest)) {
 				// build path
 				path.add(curr.getCoord());
-				while (curr.getFrom() != null) {
+				while (curr.getFrom() != null && !curr.getFrom().getCoord().equals(start)) {
 					path.add(curr.getFrom().getCoord());
 					curr = curr.getFrom();
 				}

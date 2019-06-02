515ddc0cd405a240d916809eab4333072c2a4ee7
Zane Duffield <z.duffield@student.unimelb.edu.au>
2019-06-02 21:24:19 +1000

    moved path related methods up into MyAutoController

--- src/mycontroller/MyAutoController.java
+++ src/mycontroller/MyAutoController.java
@@ -5,8 +5,10 @@ import exceptions.UnsupportedModeException;
 import swen30006.driving.Simulation;
 import world.Car;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Queue;
 
 import tiles.LavaTrap;
 import tiles.MapTile;
@@ -23,6 +25,10 @@ public class MyAutoController extends CarController {
 		private boolean isBackingFromWall = false;
 		private boolean isDrivingFromWall = false;
 
+		private ArrayList<Coordinate> currentPath = null;
+		private int nextInPath = 0;
+		private Coordinate dest;
+		private boolean isHeadingToFinish = false;
 		
 		// stores the locations of the exit tiles
 		protected ArrayList<Coordinate> finish = new ArrayList<Coordinate>();
@@ -338,5 +344,82 @@ public class MyAutoController extends CarController {
 		public void applyBrake(){
 			super.applyBrake();
 			this.isGoingForward = true;
+		}
+		
+		private ArrayList<Coordinate> findPath(Coordinate start, Coordinate dest, ArrayList<Coordinate> coordsToAvoid){
+			// perform BFS on the map
+			ArrayList<Coordinate> path = new ArrayList<>();
+			HashMap<Coordinate,MapTile> map = this.getMap();
+			HashMap<Coordinate, Node> node_map = new HashMap<>();
+			for (Coordinate coord: map.keySet()) {
+				node_map.put(coord, new Node(coord, null));
+			}
+			
+			Queue<Node> queue = new ArrayDeque<>();
+			Node start_node = node_map.get(start);
+			start_node.setDiscovered(true);
+			queue.add(start_node);
+			
+			while(queue.size() > 0) {
+				Node curr = queue.remove();
+				if (curr.getCoord().equals(dest)) {
+					// build path
+					path.add(curr.getCoord());
+					while (curr.getFrom() != null && !curr.getFrom().getCoord().equals(start)) {
+						path.add(curr.getFrom().getCoord());
+						curr = curr.getFrom();
+					}
+					// we're done, break and return path
+					break;
+				}
+				
+				// find all valid adjacent tiles to curr
+				int x = curr.getCoord().x;
+				int y = curr.getCoord().y;
+				
+				int[][] xy_changes = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+				for (int[] dxdy: xy_changes) {
+					Coordinate next_coord = new Coordinate(x + dxdy[0], y + dxdy[1]);
+					MapTile tile = map.get(next_coord);
+					
+					// if the coord corresponds to a non-wall tile
+					if (tile != null && !tile.isType(MapTile.Type.WALL)
+							&& !coordsToAvoid.contains(next_coord)) {
+						Node next_node = node_map.get(next_coord);
+						// if undiscovered and is a drivable tile
+						if (! next_node.isDiscovered()) {
+							next_node.setDiscovered(true);
+							next_node.setFrom(curr);
+							queue.add(next_node);
+						}
+					}
+				}
+			}
+			return path;
+		}
+		
+		private void setPath(ArrayList<Coordinate> path) {
+			assert(path != null);
+			
+			this.applyBrake();
+			this.currentPath = path;
+			this.nextInPath = 0;
+			
+			
+			System.out.println("Path set towards: " + path.get(0));
+			System.out.println(path.toString());
+			
+			updateDest();
+		}
+		
+		private void resetPath() {
+			this.applyBrake();
+			this.currentPath = null;
+			this.nextInPath = 0;
+			System.out.println("Path reset");
+		}
+		
+		private void updateDest() {
+			this.dest = currentPath.get(currentPath.size() - nextInPath - 1);
 		}
 	}
